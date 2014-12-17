b9225ecf7552a6754a3fb8002d46f52c44c08e6a
Emmanuel <emm_vandenbergh@hotmail.com>
2014-12-17 10:34:32 +0100

    bijna

---
+++ MasterCode/Code/SplashCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashCamera : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				if (transform.position.x > 60)
+						transform.position -= new Vector3 (1, 0, 0) * Time.deltaTime;
+		}
+}
