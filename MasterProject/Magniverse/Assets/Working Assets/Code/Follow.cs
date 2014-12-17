001ebe4bcf97071d6cad1fcf2660fe902b5d2665
Emmanuel <emm_vandenbergh@hotmail.com>
2014-12-17 10:59:04 +0100

    Add Master Project and Delete Master Builds

---
+++ MasterProject/Magniverse/Assets/Working Assets/Code/Follow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+	public Vector3 offset;
+	public Transform target;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, target.position + offset, Time.deltaTime);
+	}
+}
+
