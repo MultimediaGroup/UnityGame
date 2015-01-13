cda9441fa37310e4426d111f5f6a3f4ec95c49dc
dmalef <dmalef@hotmail.com>
2015-01-13 11:48:58 +0100

    Master Slingshot Project

---
+++ SlingshotMaster/Assets/Assets/Scripts/MagScript2.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagScript2 : MonoBehaviour {
+
+	public float initV;
+	public GameObject planets;
+
+	private bool pos = true;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce (initV * new Vector3(0,0,1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space) || Input.touches.Length > 0) {
+			pos = !pos;
+		}
+		if (pos) {
+			foreach (Transform planet in planets.transform) {
+				PlanetScript ps =  planet.GetComponent<PlanetScript>();
+				rigidbody.AddForce(ps.mass / (planet.position - transform.position).magnitude * (planet.position - transform.position).normalized * Time.deltaTime);
+			}	
+		}else{
+			foreach (Transform planet in planets.transform) {
+				PlanetScript ps =  planet.GetComponent<PlanetScript>();
+				rigidbody.AddForce(ps.mass / (planet.position - transform.position).magnitude * -1 * (planet.position - transform.position).normalized * Time.deltaTime);
+			}
+		}
+	}
+}
