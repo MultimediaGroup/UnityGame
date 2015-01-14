3de38f373f9e74808ad7a85f28f43497806eebc0
Lode Lesage <musilitar@gmail.com>
2015-01-14 17:24:29 +0100

    Sound class + sound files

---
+++ AssetsMusilitar/Working Assets/Scripts/Sound.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+	public static AudioSource main;
+
+	// Use this for initialization
+	void Start () {
+		main = audio;
+		startMusic(2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public static void playCollision() {
+		AudioClip clip = Resources.Load("collision1") as AudioClip;
+		main.PlayOneShot(clip);
+	}
+	
+	public static void playMenu() {
+		AudioClip clip = Resources.Load("menu1") as AudioClip;
+		main.PlayOneShot(clip);
+	}
+	
+	public static void startMusic(int track = 1) {
+		AudioClip clip = Resources.Load("music" + track) as AudioClip;
+		main.clip = clip;
+		main.Play();
+	}
+	
+	public static void stopPlaying() {
+		main.Stop();
+	}
+}
