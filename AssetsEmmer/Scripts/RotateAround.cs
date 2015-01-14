5fec70368eb3e902e8766ebda5295192a33b3caa
Emmanuel <emm_vandenbergh@hotmail.com>
2015-01-14 22:30:38 +0100

    Nieuw Spinscript + Splash + Scorebord + Collectable

---
+++ AssetsEmmer/Scripts/RotateAround.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+
+	public GameObject planet;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(planet.transform.position, Vector3.up, 20 * Time.deltaTime);
+	}
+}
