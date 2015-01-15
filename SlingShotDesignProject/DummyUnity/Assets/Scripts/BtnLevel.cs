40f5f57d0f6aa91d0800b4f900a841d32aa9d665
Emmanuel <emm_vandenbergh@hotmail.com>
2015-01-15 10:07:19 +0100

    Update DesignProject

---
+++ SlingShotDesignProject/DummyUnity/Assets/Scripts/BtnLevel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnLevel : MonoBehaviour {
+
+	public GameObject menu;
+
+	// Use this for initialization
+	void Start () {
+		menu.renderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnMouseDown () {
+		if (Input.GetKey ("mouse 0")) {
+			menu.renderer.enabled = true;
+		}
+	}
+}
---
+++ SlingShotDesignProject/DummyUnity/Assets/Scripts/BtnSplash.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnSplash : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnMouseDown () {
+		if (Input.GetKey ("mouse 0")) {
+			Application.LoadLevel("level1");
+		}
+	}
+
+}
