d4720544bc34d1066ec8c7fb596cb97c9360ea27
Emmanuel <emm_vandenbergh@hotmail.com>
2015-01-15 09:49:52 +0100

    Design Project toegevoegd

---
+++ SlingShotDesignProject/DummyUnity/Assets/Scripts/SpinScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinScript : MonoBehaviour {
+
+	public int min = 10;
+	public int max = 20;
+	public bool left = false;
+
+	private int speedVertical;
+	private int speedHorizontal;
+	
+
+	// Use this for initialization
+	void Start () {
+		speedVertical = Random.Range(5, 10);
+		speedHorizontal = Random.Range(5, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up, speedVertical * Time.deltaTime);
+		if (left) transform.Rotate(Vector3.left, speedHorizontal * Time.deltaTime);
+		else transform.Rotate(Vector3.right, speedHorizontal * Time.deltaTime);
+	}
+}
